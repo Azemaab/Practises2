7d6d22503b751247bcc9034978fb09d64aa72a6e
azemaab <azemaabdybekova@gmail.com>
2026-02-09 00:21:21 +0600

    Task 21

---
+++ Tasks1to5/Practises6to10/Tasks11to15/Task21.java
@@ -0,0 +1,29 @@
+package Tasks1to5.Practises6to10.Tasks11to15;
+
+import java.util.*;
+
+public class Task21 {
+
+    static ArrayDeque<String> tasks = new ArrayDeque<>();
+
+    public static void addTask(String task, boolean highPriority) {
+        if (highPriority) {
+            tasks.addFirst(task);
+        } else {
+            tasks.addLast(task);
+        }
+    }
+
+    public static void processTask() {
+        System.out.println("Processing: " + tasks.pollFirst());
+    }
+
+    public static void main(String[] args) {
+        addTask("Homework", false);
+        addTask("Exam", true);
+        addTask("Project", false);
+
+        processTask();
+        processTask();
+    }
+}
